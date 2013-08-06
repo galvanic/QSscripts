209e056a703d10dc50d3b559ff32f1d2b8c44229
Justine Courty <justinecourty@yahoo.fr>
2013-08-06 14:30:11 +0200

    Took yaxis labels and and ticks off, it looks better now. However, I am changing the plot type to make an interval bar plot, more suited for this data.

--- comptime.py
+++ comptime.py
@@ -58,7 +58,7 @@ def parseTime(filename):
 	return str2datetime(datetime_strings), word2bool(activity)
 
 
-def makeGraph(data):
+def makePlot(data):
 	"""
 	Make a graph of Time vs. Activity
 
@@ -95,19 +95,58 @@ def makeGraph(data):
 
 	for label in ax.get_xticklabels():
 		label.set_horizontalalignment('left')
-	# pylab.show()
+	ax.set_yticks([])
+	# pylab.show()git 
 	pylab.savefig("plot1.png", bbox_inches=0)
 
 	return
 
 
+def makeBarPlot():
+	import datetime
+	import numpy as np
+	import matplotlib.dates as mdates
+	import matplotlib.pyplot as plt
+
+	# dates for xaxis
+	event_date = [datetime.datetime(2008, 12, 3), datetime.datetime(2009, 1, 5), datetime.datetime(2009, 2, 3)]
+
+	# base date for yaxis can be anything, since information is in the time
+	anydate = datetime.date(2001,1,1)
+
+	# event times
+	event_start = [datetime.time(20, 12), datetime.time(12, 15), datetime.time(8, 1,)]
+	event_finish = [datetime.time(23, 56), datetime.time(16, 5), datetime.time(18, 34)]
+
+	# translate times and dates lists into matplotlib date format numpy arrays
+	start = np.fromiter((mdates.date2num(datetime.datetime.combine(anydate, event)) for event in event_start), dtype = 'float', count = len(event_start))
+	finish = np.fromiter((mdates.date2num(datetime.datetime.combine(anydate, event)) for event in event_finish), dtype = 'float', count = len(event_finish))
+	date = mdates.date2num(event_date)
+
+	# calculate events durations
+	duration = finish - start
+
+	fig = plt.figure()
+	ax = fig.add_subplot(1, 1, 1)
+
+	# use errorbar to represent event duration
+	ax.errorbar(date, start, [np.zeros(len(duration)), duration], linestyle = '')
+	# make matplotlib treat both axis as times
+	ax.xaxis_date()
+	ax.yaxis_date()
+
+	plt.show()
+	return
+
+
 def main():
 	data = parseTime(FILE)
-	makeGraph(data)
+	makePlot(data)
 	return
 
 if __name__ == "__main__":
 
-	sys.exit(main())
+	# sys.exit(main())
+	makeBarPlot()
 
 
